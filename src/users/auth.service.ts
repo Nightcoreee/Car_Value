45adb56100b46ab44058b2aa501e2e54342166c1
Hà Đào <94346501+Nightcoreee@users.noreply.github.com>
2026-01-20 22:43:46 +0700

    Add authentication signin user

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Injectable } from "@nestjs/common";
+import { BadRequestException, Injectable, NotFoundException } from "@nestjs/common";
 import { UsersService } from "./users.service";
 import { randomBytes, scrypt as _scrypt } from "crypto";
 import { promisify } from "util";
@@ -13,7 +13,7 @@ export class AuthService {
         //See if email in use
         const users = await this.usersService.find(email);
         if (users.length) {
-            throw new BadRequestException('email in use');
+            throw new BadRequestException('email in use');  
         }
 
         //Hash the users password
@@ -33,5 +33,18 @@ export class AuthService {
         return user;
     }
 
-    signin() {}
+    async signin(email: string, password: string) {
+        const [user] = await this.usersService.find(email);
+        if (!user) {
+            throw new NotFoundException('user not found');
+        }
+
+        const [salt, storedHash] = user.password.split('.');
+        const hash = (await scrypt(password, salt, 32)) as Buffer;
+
+        if (storedHash !== hash.toString('hex')) {
+            throw new BadRequestException('bad password');
+        }
+        return user;
+    }
 }
