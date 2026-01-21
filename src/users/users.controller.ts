d78b5586b4889b5ae2e7428dc17d751faa81f13e
Hà Đào <94346501+Nightcoreee@users.noreply.github.com>
2026-01-21 23:48:49 +0700

    Add signout user

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -20,6 +20,11 @@ export class UsersController {
     whoAmI(@Session() session: any) {
         return this.usersService.findOne(session.userId);
     }
+
+    @Post('/signout')
+    signOut(@Session() session: any) {
+        session.userId = null;
+    }
   
     @Post('/signup')
     async createUser(@Body() body: CreateUserDto, @Session() session: any) {
