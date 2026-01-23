234f7272ed8316e263c6abc0070706417d47f02f
Hà Đào <94346501+Nightcoreee@users.noreply.github.com>
2026-01-23 14:21:06 +0700

    Unit test auth service

---
+++ src/users/auth.service.spec.ts
@@ -0,0 +1,26 @@
+import { Test } from '@nestjs/testing';
+import { UsersService } from './users.service';
+import { AuthService } from './auth.service';
+
+//test case
+it('can create an instance of AuthService', async () => {
+    //create a fake copy of users service
+    const fakeUsersService = {
+        find: () => Promise.resolve([]),
+        create: (email: string, password: string) => Promise.resolve({ id: 1, email, password } ),
+    };
+
+
+    const module = await Test.createTestingModule({
+        providers: [AuthService, 
+            { 
+                provide: UsersService,
+                useValue: fakeUsersService 
+            }
+        ],
+    }).compile();
+
+    const service = module.get(AuthService);
+    expect(service).toBeDefined();
+});
+
