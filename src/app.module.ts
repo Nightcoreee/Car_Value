a61d13096b4d945ab10c3177fd456063cef883a8
Hà Đào <94346501+Nightcoreee@users.noreply.github.com>
2026-01-29 06:17:06 +0700

    setup db for test

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,17 +7,31 @@ import { UserEntity } from './users/user.entity';
 import { ReportEntity } from './reports/report.entity';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 const cookieSession = require('cookie-session');
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'db.sqlite',
-      entities: [UserEntity, ReportEntity], 
-      synchronize: true,
-  }), 
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+          type: 'sqlite',
+          database: configService.get<string>('DB_NAME'),
+          synchronize: true,
+          entities: [UserEntity, ReportEntity],
+      })
+    }),
+  //   TypeOrmModule.forRoot({
+  //     type: 'sqlite',
+  //     database: 'db.sqlite',
+  //     entities: [UserEntity, ReportEntity], 
+  //     synchronize: true,
+  // }), 
     UsersModule,
     ReportsModule,
     ], 
